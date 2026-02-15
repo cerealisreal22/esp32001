0513f33c6fab1891db7e39b0a05efcdae3adf7cb
cerealisreal22 <naphawan.wan2514@gmail.com>
2026-02-15 17:42:01 +0700

    server.py

--- server.py
+++ server.py
@@ -67,4 +67,10 @@ def upload():
             last_seen = None
             telegram_sent = False
 
+
     return "OK"
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
